0de786b34a1d5c2368a78fbcf49a3deacd0208f4
aishakumari10 <ak10aisha@gmail.com>
2026-01-22 11:54:57 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+///const container=document.querySelector('.contaier')
+///const outer=document.querySelector('.outer')
+///const button=document.querySelector('.button')
+
+
+///container.addEventListener('click',()=>{console.log('div')},true)
+///outer.addEventListener('click',()=>{console.log("outer Div")},true)
+///button.addEventListener('click',()=>{console.log("button")},true)
+
+//console.log()
+console.log(b)
+
+//console.log(a)
+
+let a=237382
+var b=3764
+
+
+console.log(a)
+console.log(b)
+function print(){
+    console.log("inside fn")
+}
+
+print()
---
+++ python.py
@@ -0,0 +1,13 @@
+'''arr=[3,4,7,6]
+i=0
+j=len(arr)-1
+while i<j:
+    arr[i],arr[j]=arr[j],arr[i]'''
+
+n=int(inpu('enter a number: '))
+if n>1:
+    if n//2==0:
+        print("it is a prime number.")
+        break
+    else:
+        print("it is not a prime number.")
